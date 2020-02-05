ac5c0b518541f46d4960b2bd1beaf51d5dd30234
SaintJohn-Royce <60101714+SaintJohn-Royce@users.noreply.github.com>
2020-02-05 15:03:37 +0800

    Update celsiusToFahrenheit.py

--- celsiusToFahrenheit.py
+++ celsiusToFahrenheit.py
@@ -4,4 +4,4 @@ celsius = int(celsius)
 
 fahrenheit = celsius * (9/5) + 32
 
-print('this many', str(celsius), 'sdegrees celsius is', str(fahrenheit), 'degrees Fahrenheit')
+print('this many', str(celsius), 'degrees celsius is', str(fahrenheit), 'degrees Fahrenheit')
