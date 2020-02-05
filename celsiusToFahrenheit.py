78eabf0e4bf3633fc326ba9ccdfeef89df4d4be8
Saint-John Royce <vzhu6682@uni.sydney.edu.au>
2020-02-05 15:02:22 +0800

    Add new file

---
+++ celsiusToFahrenheit.py
@@ -0,0 +1,7 @@
+celsius = input('please input the temperature in celsius: ')
+
+celsius = int(celsius)
+
+fahrenheit = celsius * (9/5) + 32
+
+print('this many', str(celsius), 'sdegrees celsius is', str(fahrenheit), 'degrees Fahrenheit')
